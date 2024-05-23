ca121056340958531524d731edcd11b2eb6a57ba
renato_chagas <renatochagas.m@gmail.com>
2024-05-22 23:43:02 -0300

    new design forgotpass

--- src/views/login-signup/ForgotPassView.vue
+++ src/views/login-signup/ForgotPassView.vue
@@ -5,34 +5,31 @@ import Logogeekours from '../../components/icons/logogeekours.vue';
 <template>
     <main class="flex justify-center items-center w-screen h-screen">
         <div v-if="true" class="flex flex-col w-2/5 h-2/5 shadow-3xl rounded-2xl">
-            <div class="flex justify-center h-20/1">
-                <Logogeekours class="h-16" />
+            <div class="flex justify-center w-full p-4">
+                <Logogeekours class="w-48" />
             </div>
-            <div class="flex flex-row h-full w-full ">
-                <div class="flex flex-col h-full w-2/5">
-                    <div class="">
-                        <h3 class="txt-rec-pass text-xl text-center">
-                            Recupere sua senha
-                        </h3>
-                    </div>
-                    <div class="flex justify-center items-center ">
-                        <h6 class="text-base">
-                            Informe seu email a seguir para recuperar sua senha. Este email pode demorar de 10 a 30
-                            minutos
-                            para chegar em sua caixa de entrada.
-                        </h6>
+            <div class="flex h-full">
+                <div class="h-full w-1/2">
+                    <div class="h-full w-full">
+                        <div class="flex justify-center w-full mt-8">
+                            <h3>Recupere sua senha</h3>
+                        </div>
+                        <div class="flex justify-center p-4 mt-3">
+                            <p>Insira seu email para recuperar sua senha. Este processo pode demorar de 10 a 15 minutos
+                                para o email de recuperação chegar na sua caixa de entrda.</p>
+                        </div>
+                        <div class="flex justify-center p-4">
+                            <p>Caso esteja com problemas, contate nos via email,
+                                <a class="supmail" href="mailto: support@geekours.com" >support@geekours.com .</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="h-full w-3/5 ">
-                    <div class="h-full w-full ">
-                        <form action="" class="flex flex-col">
-                            <label for="" class="w-full">Email</label>
-                            <input type="email" class="border rounded-md h-10 w-full pl-3 border-2 border-gray-700"
-                                placeholder="Digite seu email" />
-                            <div class="send-btn w-full">
-                                <button class="bg-blue-500 text-white rounded-md h-10 w-20 ">Enviar</button>
-                            </div>
-                        </form>
+                <div class="flex h-full w-1/2">
+                    <div class="flex flex-col my-16 mx-auto p-10">
+                        <label for="">Email</label>
+                        <input placeholder="Digite seu email" type="text"
+                            class="border-2 w-90/1 border-gray-600 rounded-lg pl-2">
                     </div>
                 </div>
             </div>
@@ -44,4 +41,8 @@ import Logogeekours from '../../components/icons/logogeekours.vue';
     </main>
 </template>
 
-<style></style>
+<style scoped>
+.supemail{
+    color: blue;
+}
+</style>
